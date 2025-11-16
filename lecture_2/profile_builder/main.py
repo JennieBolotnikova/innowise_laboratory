a8125cf81f8cbe29fb946425bd617b6801a281de
JennieBolotnikova <79278336+JennieBolotnikova@users.noreply.github.com>
2025-11-16 20:55:08 +0300

    Mini-profile generator added

---
+++ lecture_2/profile_builder/main.py
@@ -0,0 +1,85 @@
+from datetime import datetime
+
+def generate_profile(age: int):
+    if 0 <= age <= 12:
+        return "Child"
+    elif 13 <= age <= 19:
+        return "Teenager"
+    elif age >= 20:
+        return "Adult"
+    else:
+        return "Invalid age"
+
+def get_user_name():
+    name = input("Enter your full name: ")
+    return name
+
+def get_user_age():
+    while True:
+        try:
+            birth_year_str = input("Enter your birth year: ")
+            birth_year = int(birth_year_str)
+            current_year = datetime.now().year
+            if birth_year < 1905:
+                print("Error: Birth year cannot be earlier than 1905. Please try again.")
+                continue
+
+            if birth_year > current_year:
+                print("Error: Birth year cannot be in the future. Please try again.")
+                continue
+            current_age = current_year - birth_year
+
+            return birth_year, current_age
+        except ValueError:
+            print("Error: Please enter a valid integer for the birth year. Please try again.")
+
+
+def get_hobbies():
+    hobbies = list()
+    while True:
+        answer = input("Enter a favorite hobby or type 'stop' to finish: ")
+        if answer == "stop":
+            break
+        else:
+            hobbies.append(answer)
+    return hobbies
+
+def create_user_profile(name, age, hobbies):
+    life_stage = generate_profile(age)
+    user_profile = {"name": name,
+                    "age": age,
+                    "stage": life_stage,
+                    "hobbies": hobbies}
+    return user_profile
+
+
+def display_profile(user_profile):
+    print("---")
+    print("Profile Summary")
+    print(f"Name: {user_profile["name"]}")
+    print(f"Age: {user_profile["age"]}")
+    print(f"Life stage: {user_profile["stage"]}")
+
+    if len(user_profile["hobbies"]) == 0:
+        print("You didn't mention any hobbies")
+    else:
+        print(f"Favorite hobbies ({len(user_profile["hobbies"])})")
+        for hobby in user_profile["hobbies"]:
+            print(f"- {hobby}")
+
+    print("---")
+
+def main():
+    user_name = get_user_name()
+    birth_year_str, current_age = get_user_age()
+    hobbies = get_hobbies()
+    user_profile = create_user_profile(user_name, current_age, hobbies)
+    display_profile(user_profile)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
